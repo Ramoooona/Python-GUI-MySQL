3dd3fc4494c02b482bde0e67169c8e8a8a5a1549
Ramona(Yuanmeng) Zhang <yuanmengzh@gmail.com>
2016-12-08 20:07:30 -0500

    Source code for application

--- retailFoodStore.py
+++ retailFoodStore.py
@@ -1,8 +1,6 @@
 
 # coding: utf-8
 
-# In[20]:
-
 import tkinter
 from tkinter import *
 import mysql.connector as mc
@@ -63,49 +61,102 @@ class App(Frame):
     
     def create(self):
         # Create table 'deficiency'
-        self.cur.execute("CREATE TABLE IF NOT EXISTS deficiency (deficiencyCode CHAR(3) NOT NULL,                         description TEXT NOT NULL,PRIMARY KEY (deficiencyCode))                          COLLATE='utf8_general_ci'ENGINE=InnoDB")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS deficiency (\
+                         deficiencyCode CHAR(3) NOT NULL,\
+                         description TEXT NOT NULL,PRIMARY KEY (deficiencyCode)) COLLATE='utf8_general_ci'ENGINE=InnoDB")
         
         # Create table 'zipcode'
-        self.cur.execute("CREATE TABLE IF NOT EXISTS zipcode (                          zipCode CHAR(5) NOT NULL,                          city VARCHAR(50) NOT NULL,                          county VARCHAR(50) NOT NULL,PRIMARY KEY (zipCode))                          COLLATE='utf8_general_ci'ENGINE=InnoDB")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS zipcode (\
+                         zipCode CHAR(5) NOT NULL,\
+                         city VARCHAR(50) NOT NULL,\
+                         county VARCHAR(50) NOT NULL,PRIMARY KEY (zipCode))\
+                         COLLATE='utf8_general_ci'ENGINE=InnoDB")
         
         # Create table stores
-        self.cur.execute("CREATE TABLE IF NOT EXISTS stores (                         licenseNo VARCHAR(50) NOT NULL,                         establishment VARCHAR(10) NOT NULL,                         entityName VARCHAR(50) NOT NULL,                         DBAname VARCHAR(50) NOT NULL,                         squareFootage VARCHAR(50) NOT NULL,                         zip CHAR(5) NOT NULL,PRIMARY KEY (licenseNo),                         INDEX FK_stores_zipcode (zip),                         CONSTRAINT FK_stores_zipcode FOREIGN KEY (zip) REFERENCES zipcode (zipCode)                          ON DELETE NO ACTION)COLLATE='utf8_general_ci'ENGINE=InnoDB")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS stores (\
+                         licenseNo VARCHAR(50) NOT NULL,\
+                         establishment VARCHAR(10) NOT NULL,\
+                         entityName VARCHAR(50) NOT NULL,\
+                         DBAname VARCHAR(50) NOT NULL,\ 
+                         squareFootage VARCHAR(50) NOT NULL,\
+                         zip CHAR(5) NOT NULL,PRIMARY KEY (licenseNo),\
+                         INDEX FK_stores_zipcode (zip),\
+                         CONSTRAINT FK_stores_zipcode FOREIGN KEY (zip) REFERENCES zipcode (zipCode)ON DELETE NO ACTION)\
+                         COLLATE='utf8_general_ci'ENGINE=InnoDB")
         
         # Create table violations
-        self.cur.execute("CREATE TABLE IF NOT EXISTS violations (                         id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,                         inspectionDate DATE NOT NULL,                         deficiencyCode CHAR(3) NOT NULL,                         licenseNo VARCHAR(50) NOT NULL,PRIMARY KEY (id),                         INDEX FK_violations_stores (licenseNo),                         INDEX FK_violations_deficiency (deficiencyCode),                         CONSTRAINT FK_violations_deficiency                          FOREIGN KEY (deficiencyCode) REFERENCES deficiency (deficiencyCode)                          ON DELETE NO ACTION,                         CONSTRAINT FK_violations_stores                          FOREIGN KEY (licenseNo) REFERENCES stores (licenseNo)                          ON DELETE NO ACTION)COLLATE='utf8_general_ci'ENGINE=InnoDB")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS violations (\
+                         id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,\ 
+                         inspectionDate DATE NOT NULL, \
+                         deficiencyCode CHAR(3) NOT NULL,\
+                         licenseNo VARCHAR(50) NOT NULL,PRIMARY KEY (id),\
+                         INDEX FK_violations_stores (licenseNo), \
+                         INDEX FK_violations_deficiency (deficiencyCode),\
+                         CONSTRAINT FK_violations_deficiency \
+                         FOREIGN KEY (deficiencyCode) REFERENCES deficiency (deficiencyCode) ON DELETE NO ACTION, \
+                         CONSTRAINT FK_violations_stores\
+                         FOREIGN KEY (licenseNo) REFERENCES stores (licenseNo) ON DELETE NO ACTION)\
+                         COLLATE='utf8_general_ci'ENGINE=InnoDB")
         
     def importdata(self):
         self.cur.execute("SET FOREIGN_KEY_CHECKS=0")
         
-        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'deficiency.csv'         REPLACE INTO TABLE retailfoodstore.deficiency CHARACTER SET utf8         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' IGNORE 1 LINES")
+        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'deficiency.csv' \
+                         REPLACE INTO TABLE retailfoodstore.deficiency CHARACTER SET utf8\
+                         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' \
+                         IGNORE 1 LINES")
         
-        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'zip.csv'         REPLACE INTO TABLE retailfoodstore.zipcode CHARACTER SET utf8         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' IGNORE 1 LINES")
+        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'zip.csv'\
+                         REPLACE INTO TABLE retailfoodstore.zipcode CHARACTER SET utf8\
+                         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' \
+                         IGNORE 1 LINES")
         
-        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'stores.csv'         REPLACE INTO TABLE retailfoodstore.stores CHARACTER SET utf8         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' IGNORE 1 LINES")
+        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'stores.csv'\
+                         REPLACE INTO TABLE retailfoodstore.stores CHARACTER SET utf8\
+                         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' \
+                         IGNORE 1 LINES")
         
-        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'violations.csv'         REPLACE INTO TABLE retailfoodstore.violations CHARACTER SET utf8         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' IGNORE 1 LINES")
+        self.cur.execute("LOAD DATA LOW_PRIORITY LOCAL INFILE 'violations.csv'\
+                         REPLACE INTO TABLE retailfoodstore.violations CHARACTER SET utf8 \
+                         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\\r\\n' \
+                         IGNORE 1 LINES")
         
         self.cur.execute("set FOREIGN_KEY_CHECKS=1")
      
     
     def tendef(self):
-        self.cur.execute('SELECT count AS "Number of Violations", description AS "Deficiency" FROM deficiency d,(SELECT deficiencyCode, COUNT(deficiencyCode) AS count FROM violations GROUP BY deficiencyCode ORDER BY count DESC LIMIT 10) a WHERE d.deficiencyCode=a.deficiencyCode')
+        self.cur.execute('SELECT count AS "Number of Violations", description AS "Deficiency" \
+                         FROM deficiency d,(SELECT deficiencyCode, COUNT(deficiencyCode) AS count \
+                         FROM violations GROUP BY deficiencyCode ORDER BY count DESC LIMIT 10) a \
+                         WHERE d.deficiencyCode=a.deficiencyCode')
         print(self.cur.fetchall())
     
     def tenenti(self):
-        self.cur.execute('SELECT DBAname AS "Store Name", county AS "County",count AS "Number of Violations" FROM stores s,zipcode z,(SELECT licenseNo, COUNT(licenseNo) AS count FROM violations WHERE inspectionDate<"2016-10-25" AND inspectionDate > "2015-10-24" GROUP BY licenseNo ORDER BY count DESC LIMIT 10) a WHERE s.licenseNo=a.licenseNo AND s.zip=z.zipCode')
+        self.cur.execute('SELECT DBAname AS "Store Name", county AS "County",count AS "Number of Violations" \
+                         FROM stores s,zipcode z,(SELECT licenseNo, COUNT(licenseNo) AS count \
+                         FROM violations WHERE inspectionDate<"2016-10-25" AND inspectionDate > "2015-10-24" \
+                         GROUP BY licenseNo ORDER BY count DESC LIMIT 10) a WHERE s.licenseNo=a.licenseNo AND s.zip=z.zipCode')
         print(self.cur.fetchall())
         
     def avg(self):
-        self.cur.execute('SELECT * FROM (SELECT AVG(squareFootage) AS "Average Area of Stores without Violations" FROM stores s WHERE s.licenseNo NOT IN (SELECT licenseNo FROM violations)) b,(SELECT AVG(s.squareFootage) AS "Average Area of Stores with More Than 10 Times Violations" FROM stores s,(SELECT licenseNo, COUNT(licenseNo) AS count FROM violations GROUP BY licenseNo) a WHERE count>10 AND a.licenseNo=s.licenseNo) c')
+        self.cur.execute('SELECT * FROM (SELECT AVG(squareFootage) AS "Average Area of Stores without Violations" \
+                         FROM stores s WHERE s.licenseNo NOT IN (SELECT licenseNo FROM violations)) b,\
+                         (SELECT AVG(s.squareFootage) AS "Average Area of Stores with More Than 10 Times Violations" \
+                         FROM stores s,(SELECT licenseNo, COUNT(licenseNo) AS count FROM violations GROUP BY licenseNo) a \
+                         WHERE count>10 AND a.licenseNo=s.licenseNo) c')
         print(self.cur.fetchall())
     
     def citycounty(self):
-        self.cur.execute('SELECT z.city AS City,z.county AS County, s.zip, a.count AS "Number of Violations" FROM stores s, zipcode z,(SELECT licenseNo, COUNT(licenseNo) AS count FROM violations GROUP BY licenseNo ORDER BY count DESC) a WHERE s.licenseNo=a.licenseNo AND s.zip=z.zipCode')
+        self.cur.execute('SELECT z.city AS City,z.county AS County, s.zip, a.count AS "Number of Violations" \
+                         FROM stores s, zipcode z,(SELECT licenseNo, COUNT(licenseNo) AS count \
+                         FROM violations GROUP BY licenseNo ORDER BY count DESC) a \
+                         WHERE s.licenseNo=a.licenseNo AND s.zip=z.zipCode')
         print(self.cur.fetchall())
         
     def area(self):
-        self.cur.execute('SELECT entityName AS "Entity Name", DBAname AS "Store Name", city AS "City", county AS "County"                FROM stores s, zipcode z WHERE s.licenseNo NOT IN (SELECT licenseNo FROM violations)                AND s.zip=z.zipCode AND s.zip="%s"' % (self.zip.get()))
+        self.cur.execute('SELECT entityName AS "Entity Name", DBAname AS "Store Name", city AS "City", county AS "County" \
+                         FROM stores s, zipcode z WHERE s.licenseNo NOT IN (SELECT licenseNo FROM violations)\
+                         AND s.zip=z.zipCode AND s.zip="%s"' % (self.zip.get()))
         print (self.cur.fetchall())
 
 root=Tk()
